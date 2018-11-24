a591a9f4b95066a318efca43bce55a6666df87e6
nsuong <nguyensuong1918@gmail.com>
2018-11-24 10:06:19 +0700

    first commit

---
+++ WindowsFormsApp4/WindowsFormsApp4/Database.cs
@@ -0,0 +1,60 @@
+﻿using Db4objects.Db4o;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    public class Database
+    {
+        public static string DbFileName { get; set; }
+        public static IObjectContainer DB => Db4oEmbedded.OpenFile(DbFileName);
+        public static void CloseDB(IObjectContainer db)
+        {
+            db.Close();
+        }
+        public static void CreateEmployees(IObjectContainer db, string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                FileStream fs = new FileStream(fileName, FileMode.Open);
+                StreamReader fin = new StreamReader(fs);
+                int nEmps = int.Parse(fin.ReadLine());
+                for (int i = 0; i < nEmps; i++)
+                {
+                    string line = fin.ReadLine();
+                    if (line != null)
+                    {
+                        string[] fields = line.Split(':');
+                        string fname = fields[0];
+                        char minit = fields[1][0];
+                        string lname = fields[2];
+                        int ssn = int.Parse(fields[3]);
+                        string bdate = fields[4];
+                        string address = fields[5];
+                        char sex = fields[6][0];
+                        float salary = float.Parse(fields[7]);
+                        Employee e = new Employee
+                        {
+                            Ssn = ssn,
+                            FName = fname,
+                            MInit = minit,
+                            LName = lname,
+                            Address = address,
+                            BirthDate = bdate,
+                            Salary = salary,
+                            Sex = sex
+                        };
+                        db.Store(e);
+                    }
+                }
+                fin.Close();
+                fs.Close();
+            }
+        }
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    {
+ // attributes
+ public int PNumber { get; set; }
+    public string PName { get; set; }
+    public string Location { get; set; }
+    // relationships
+    public Department ControlledBy { get; set; }
+    public List<WorksOn> WorksOn { get; set; }
+}
+}

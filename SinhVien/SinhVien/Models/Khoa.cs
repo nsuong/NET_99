802c92bb93020d0ffeb9f0ebc9217f8f16638b79
khaicq <khaicq.0604@gmail.com>
2018-11-24 10:31:30 +0700

    first commit

---
+++ SinhVien/SinhVien/Models/Khoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SinhVien.Models
+{
+    public class Khoa
+    {
+        [Key]
+        public string MaKhoa { get; set; }
+
+        [Required]
+        [Display(Name ="Tên Khoa")]
+        public string TenKhoa { get; set; }
+    }
+}
---
+++ SinhVien/SinhVien/Models/SinhVien.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SinhVien.Models
+{
+    public class SinhVien
+    {
+        [Key]
+        [Required]
+        [Display(Name ="Mã sinh viên")]
+        public string MaSV { get; set; }
+        [Required]
+        [Display(Name ="Tên sinh viên")]
+        public string TenSV { get; set; }
+
+        [Display(Name ="Ngày sinh")]
+        public DateTime NgaySinh { get; set; }
+
+        [Display(Name ="Địa chỉ")]
+        public DateTime DiaChi { get; set; }
+
+        [Display(Name ="Khoa")]
+        public string MaKhoa { get; set; }
+        [ForeignKey("MaKhoa")]
+        public Khoa Khoa { get; set; }
+
+        [Display(Name ="Môn học")]
+        public string MaMon { get; set; }
+        [ForeignKey("MaMon")]
+        public MonHoc MonHoc { get; set; }
+        
+    }
+}

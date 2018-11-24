3e9bd38c3d4376d587497493e2baefa8adfba9f1
huyminh73 <hoangcongminhhuy@gmail.com>
2018-11-24 10:37:01 +0700

    first commit

---
+++ sinhvien/SinhVien/Models/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SinhVien.Models
+
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Khoa> Khoas { get; set; }
+        public DbSet<SinhVien> SinhViens { get; set; }
+        public DbSet<MonHoc> MonHocs { get; set; }
+        public DbSet<KetQua> KetQuas { get; set; }
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+    }
+}
